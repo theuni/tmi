6c27d24fd6a57b7c56c00c5f1ea3f25dd247e0dc
Cory Fields <cory-nospam-@coryfields.com>
2024-08-21 21:37:51 +0000

    add forward-declare header

---
+++ tmi_fwd.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2024 Cory Fields
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef TMI_FWD_H_
+#define TMI_FWD_H_
+namespace tmi{
+
+template <typename, typename, typename>
+class multi_index_container;
+
+} // namespace tmi
+#endif // TMI_FWD_H_
