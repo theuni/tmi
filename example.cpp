aae1d220f3d81d37d1318d6279d3431d52982210
Cory Fields <cory-nospam-@coryfields.com>
2024-10-24 17:33:34 +0000

    example cleanup

--- example.cpp
+++ example.cpp
@@ -1,11 +1,10 @@
 #include "tmi.h"
+#include <string>
 
 struct myclass {
-    size_t val{0};
+    std::string val;
     myclass() = default;
-    myclass(size_t rhs) : val(rhs) {}
-    bool operator()(const myclass&) const { return true; }
-    bool operator==(const myclass&) const = default;
+    myclass(size_t rhs) : val(std::to_string(rhs)) {}
 };
 
 struct comp_less;
@@ -16,99 +15,67 @@ struct hash_nonunique;
 class compare_myclass_less
 {
 public:
-    using sorted = std::true_type;
-    static constexpr bool sorted_type() { return true;}
-    using tag = comp_less;
-    constexpr static bool sorted_unique() { return false; }
     constexpr bool operator()(const myclass& a, const myclass& b) const noexcept
     {
-        return a.val < b.val;
+        return std::stol(a.val) < std::stol(b.val);
     }
 };
 
 class compare_myclass_greater
 {
 public:
-    using sorted = std::true_type;
-    static constexpr bool sorted_type() { return true;}
-    using tag = comp_greater;
-    constexpr static bool sorted_unique() { return true; }
     bool operator()(const myclass& a, const myclass& b) const
     {
-        return a.val > b.val;
+        return std::stol(a.val) > std::stol(b.val);
     }
 };
 
-class hash_myclass_unique
+class myclass_key_from_value
 {
 public:
-    using sorted = std::false_type;
-    static constexpr bool sorted_type() { return false;}
-    using tag = hash_unique;
-    using hash_type = size_t;
-    using result_type = size_t;
-    constexpr static bool hashed_unique() { return true; }
-    bool operator()(const myclass& a, const myclass& b) const
-    {
-        return a == b;
-    }
-    size_t operator()(const hash_type& a) const
-    {
-        return a;
-    }
-    size_t operator()(const myclass& a) const
+    using result_type = std::string;
+    result_type operator()(const myclass& a) const
     {
         return a.val;
-    }
-    bool operator()(const myclass& a, const hash_type& b) const
-    {
-        return a.val == b;
     }
 };
 
-class hash_myclass
+class myclass_hash
 {
 public:
-    using sorted = std::false_type;
-    static constexpr bool sorted_type() { return false;}
-    using tag = hash_nonunique;
-    using hash_type = size_t;
-    constexpr static bool hashed_unique() { return false; }
-    bool operator()(const myclass& a, const myclass& b) const
+    size_t operator()(const myclass_key_from_value::result_type& a) const
+    {
+        return std::hash<std::string>{}(a);
+    }
+};
+
+class myclass_pred
+{
+public:
+    bool operator()(const myclass_key_from_value::result_type& a, const myclass_key_from_value::result_type& b) const
     {
         return a == b;
-    }
-    size_t operator()(const hash_type& a) const
-    {
-        return a;
-    }
-    size_t operator()(const myclass& a) const
-    {
-        return a.val;
-    }
-    bool operator()(const myclass& a, const hash_type& b) const
-    {
-        return a.val == b;
     }
 };
 
 int main()
 {
-    tmi::multi_index_container<myclass,tmi::indexed_by<tmi::hashed_unique<tmi::tag<hash_unique>, hash_myclass_unique, hash_myclass_unique, hash_myclass_unique>,
+    tmi::multi_index_container<myclass,tmi::indexed_by<tmi::hashed_unique<tmi::tag<hash_unique>, myclass_key_from_value, myclass_hash, myclass_pred>,
                      tmi::ordered_unique<tmi::tag<comp_less>, tmi::identity<myclass>, compare_myclass_less>,
                      tmi::ordered_unique<tmi::tag<comp_greater>, tmi::identity<myclass>, compare_myclass_greater>>,
-                     std::allocator<myclass>> bar{std::make_tuple(std::make_tuple(32UL, hash_myclass_unique(), hash_myclass_unique(), hash_myclass_unique()), std::make_tuple(tmi::identity<myclass>(), compare_myclass_less()), std::make_tuple(tmi::identity<myclass>(), compare_myclass_greater()))};
+                     std::allocator<myclass>> bar{std::make_tuple(std::make_tuple(32UL, myclass_key_from_value(), myclass_hash(), myclass_pred()), std::make_tuple(tmi::identity<myclass>(), compare_myclass_less()), std::make_tuple(tmi::identity<myclass>(), compare_myclass_greater()))};
+
     auto& hash_index = bar.get<hash_unique>();
-    bar.emplace(0UL);
+    bar.emplace(0ul);
     for (unsigned i = 1; i <= 10; i++) {
         hash_index.emplace(i);
     }
-    auto it = hash_index.find(0);
+    auto it = hash_index.find("0");
     hash_index.modify(it, [](myclass& rhs) {
-        rhs.val = 11;
+        rhs.val = "11";
     });
     auto least_it = bar.get<comp_less>().begin();
     auto greatest_it = bar.get<comp_greater>().begin();
-    assert(least_it->val == 1);
-    assert(greatest_it->val == 11);
+    assert(least_it->val == "1");
+    assert(greatest_it->val == "11");
 }
