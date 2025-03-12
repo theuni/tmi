3d953855043ec2cd8e68ffdf05f3e7f0282b1e6b
Cory Fields <cory-nospam-@coryfields.com>
2025-03-12 19:30:07 +0000

    add tests for pointer types

--- example.cpp
+++ example.cpp
@@ -19,6 +19,10 @@ public:
     {
         return std::stol(a.val) < std::stol(b.val);
     }
+    TMI_CPP23_STATIC constexpr bool operator()(const myclass* a, const myclass* b) TMI_CONST_IF_NOT_CPP23_STATIC
+    {
+        return std::stol(a->val) < std::stol(b->val);
+    }
 };
 
 class compare_myclass_greater
@@ -27,6 +31,10 @@ public:
     TMI_CPP23_STATIC bool operator()(const myclass& a, const myclass& b) TMI_CONST_IF_NOT_CPP23_STATIC
     {
         return std::stol(a.val) > std::stol(b.val);
+    }
+    TMI_CPP23_STATIC bool operator()(const myclass* a, const myclass* b) TMI_CONST_IF_NOT_CPP23_STATIC
+    {
+        return std::stol(a->val) > std::stol(b->val);
     }
 };
 
@@ -37,6 +45,10 @@ public:
     TMI_CPP23_STATIC constexpr const result_type& operator()(const myclass& a) TMI_CONST_IF_NOT_CPP23_STATIC noexcept
     {
         return a.val;
+    }
+    TMI_CPP23_STATIC constexpr const result_type& operator()(const myclass* a) TMI_CONST_IF_NOT_CPP23_STATIC noexcept
+    {
+        return a->val;
     }
 };
 
@@ -60,25 +72,54 @@ public:
 
 int main()
 {
-    tmi::multi_index_container<myclass,tmi::indexed_by<tmi::hashed_unique<tmi::tag<hash_unique>, myclass_key_from_value, myclass_hash, myclass_pred>,
-                     tmi::ordered_unique<tmi::tag<comp_less>, tmi::identity<myclass>, compare_myclass_less>,
-                     tmi::ordered_unique<tmi::tag<comp_greater>, tmi::identity<myclass>, compare_myclass_greater>>,
-                     std::allocator<myclass>> bar{std::make_tuple(std::make_tuple(32UL, myclass_key_from_value(), myclass_hash(), myclass_pred()), std::make_tuple(tmi::identity<myclass>(), compare_myclass_less()), std::make_tuple(tmi::identity<myclass>(), compare_myclass_greater()))};
-
-    auto& hash_index = bar.get<hash_unique>();
-    bar.emplace(0ul);
-    for (unsigned i = 1; i <= 10; i++) {
-        hash_index.emplace(i);
+    {
+        tmi::multi_index_container<myclass,tmi::indexed_by<tmi::hashed_unique<tmi::tag<hash_unique>, myclass_key_from_value, myclass_hash, myclass_pred>,
+                         tmi::ordered_unique<tmi::tag<comp_less>, tmi::identity<myclass>, compare_myclass_less>,
+                         tmi::ordered_unique<tmi::tag<comp_greater>, tmi::identity<myclass>, compare_myclass_greater>>,
+                         std::allocator<myclass>> bar{std::make_tuple(std::make_tuple(32UL, myclass_key_from_value(), myclass_hash(), myclass_pred()), std::make_tuple(tmi::identity<myclass>(), compare_myclass_less()), std::make_tuple(tmi::identity<myclass>(), compare_myclass_greater()))};
+        auto& hash_index = bar.get<hash_unique>();
+        bar.emplace(0ul);
+        for (unsigned i = 1; i <= 10; i++) {
+            hash_index.emplace(i);
+        }
+        assert(hash_index.count("2"));
+        auto it = hash_index.find("0");
+        hash_index.modify(it, [](myclass& rhs) {
+            rhs.val = "11";
+        });
+        auto least_it = bar.get<comp_less>().begin();
+        auto nh = bar.get<comp_less>().extract(least_it);
+        auto inserted = bar.get<comp_less>().insert(std::move(nh));
+        auto greatest_it = bar.get<comp_greater>().begin();
+        assert(least_it->val == "1");
+        assert(greatest_it->val == "11");
     }
-    assert(hash_index.count("2"));
-    auto it = hash_index.find("0");
-    hash_index.modify(it, [](myclass& rhs) {
-        rhs.val = "11";
-    });
-    auto least_it = bar.get<comp_less>().begin();
-    auto nh = bar.get<comp_less>().extract(least_it);
-    auto inserted = bar.get<comp_less>().insert(std::move(nh));
-    auto greatest_it = bar.get<comp_greater>().begin();
-    assert(least_it->val == "1");
-    assert(greatest_it->val == "11");
+    // Pointer storage
+    {
+        tmi::multi_index_container<myclass*,tmi::indexed_by<tmi::hashed_unique<tmi::tag<hash_unique>, myclass_key_from_value, myclass_hash, myclass_pred>,
+                         tmi::ordered_unique<tmi::tag<comp_less>, tmi::identity<myclass*>, compare_myclass_less>,
+                         tmi::ordered_unique<tmi::tag<comp_greater>, tmi::identity<myclass*>, compare_myclass_greater>>,
+                         std::allocator<myclass*>> bar{std::make_tuple(std::make_tuple(32UL, myclass_key_from_value(), myclass_hash(), myclass_pred()), std::make_tuple(tmi::identity<myclass*>(), compare_myclass_less()), std::make_tuple(tmi::identity<myclass*>(), compare_myclass_greater()))};
+        auto& hash_index = bar.get<hash_unique>();
+        std::array<myclass, 11> arr;
+        arr[0] = 0;
+        bar.emplace(&arr[0]);
+        for (unsigned i = 1; i <= 10; i++) {
+            arr[i] = i;
+            hash_index.emplace(&arr[i]);
+        }
+        assert(hash_index.count("2"));
+        auto it = hash_index.find("0");
+        hash_index.modify(it, [](myclass* rhs) {
+            rhs->val = "11";
+        });
+        auto least_it = bar.get<comp_less>().begin();
+        auto nh = bar.get<comp_less>().extract(least_it);
+        auto inserted = bar.get<comp_less>().insert(std::move(nh));
+        auto greatest_it = bar.get<comp_greater>().begin();
+        assert((*least_it)->val == "1");
+        assert((*greatest_it)->val == "11");
+        auto reverse_it = bar.iterator_to(&arr[0]);
+        assert((*reverse_it)->val == "11");
+    }
 }
